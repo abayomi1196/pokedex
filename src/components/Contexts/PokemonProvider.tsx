2e59a6cf3013a054dadb2c9ed327f5d68bd7fc55
Abayomi Ayodele <samabayomi301@gmail.com>
2022-11-19 01:32:08 +0100

    Initial commit

--- src/components/Contexts/PokemonProvider.tsx
+++ src/components/Contexts/PokemonProvider.tsx
@@ -1,30 +1,36 @@
-import React, { useContext, useEffect, useState } from 'react'
-import PokeAPI, { INamedApiResource, IPokemon } from 'pokeapi-typescript'
-import { getIdFromUrl, isOG } from '../../utils'
+import React, { useContext, useEffect, useState, useCallback } from "react";
+import PokeAPI, { INamedApiResource, IPokemon } from "pokeapi-typescript";
+import { getIdFromUrl, isOG } from "../../utils";
 
 export enum Field {
-  favourite = 'favourite',
+  favourite = "favourite",
+  type = "type"
 }
 
-type Filters = { [key in Field]: FilterValue }
-type FilterValue = boolean | string | string[] | undefined
+type Filters = { [key in Field]: FilterValue };
+type FilterValue = boolean | string | string[] | undefined;
 
 interface PokemonContextData {
-  pokemon: INamedApiResource<IPokemon>[]
-  query: string
-  search: (query: string) => void
-  favourites: string[]
-  addFavourite: (pokemon: INamedApiResource<IPokemon>) => void
-  removeFavourite: (pokemon: INamedApiResource<IPokemon>) => void
-  filters: Filters
-  addFilter: (field: Field, value: FilterValue) => void
-  removeFilter: (field: Field) => void
+  pokemon: INamedApiResource<IPokemon>[];
+  query: string;
+  selectedTypes: PokemonType[];
+  selectType: (type: PokemonType) => void;
+  search: (query: string) => void;
+  favourites: string[];
+  addFavourite: (pokemon: INamedApiResource<IPokemon>) => void;
+  removeFavourite: (pokemon: INamedApiResource<IPokemon>) => void;
+  filters: Filters;
+  addFilter: (field: Field, value: FilterValue) => void;
+  removeFilter: (field: Field) => void;
+  types: PokemonType[];
 }
 
-export const PokemonContext = React.createContext<PokemonContextData | undefined>(undefined)
+export const PokemonContext = React.createContext<
+  PokemonContextData | undefined
+>(undefined);
 
 interface PokemonProviderProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export enum PokemonType {
@@ -45,7 +51,7 @@ export enum PokemonType {
   psychic = "psychic",
   rock = "rock",
   steel = "steel",
-  water = "water",
+  water = "water"
 }
 
 export enum PokemonStat {
@@ -58,127 +64,195 @@ export enum PokemonStat {
 }
 
 const PokemonProvider: React.FC<PokemonProviderProps> = ({ children }) => {
-  const [data, setData] = useState<INamedApiResource<IPokemon>[]>()
-  const [pokemon, setPokemon] = useState<INamedApiResource<IPokemon>[]>()
-  const [favourites, setFavourites] = useState<string[]>([])
-  const [query, setQuery] = useState<string>('')
-  const [filters, setFilters] = useState<Filters>({} as Filters)
-  const [error, setError] = useState<any>()
+  const [data, setData] = useState<INamedApiResource<IPokemon>[]>();
+  const [pokemon, setPokemon] = useState<INamedApiResource<IPokemon>[]>();
+  const [favourites, setFavourites] = useState<string[]>([]);
+  const [types, setTypes] = useState<PokemonType[]>([]);
+  const [selectedTypes, setSelectedTypes] = useState<PokemonType[]>([]);
+  const [query, setQuery] = useState<string>("");
+  const [filters, setFilters] = useState<Filters>({} as Filters);
+  const [error, setError] = useState<any>();
 
   useEffect(() => {
-    fetchPokemon()
-  }, [])
+    fetchPokemon();
+  }, []);
 
   useEffect(() => {
-    filterData()
-  }, [filters, query, data])
+    fetchPokemonTypes();
+  }, []);
+
+  useEffect(() => {
+    filterData();
+  }, [filters, query, selectedTypes]);
 
   const filterData = async () => {
     if (!data) {
-      return
+      return;
     }
 
-    let filteredData = [...data]
-    const fields = Object.keys(filters) as Field[]
+    let filteredData = [...data];
+    const fields = Object.keys(filters) as Field[];
 
     for (const field of fields) {
       switch (field) {
         case Field.favourite: {
-          const value = filters[field]
+          const value = filters[field];
           if (value) {
-            filteredData = filteredData.filter((pokemon) => favourites.includes(pokemon.name))
+            filteredData = filteredData.filter((pokemon) =>
+              favourites.includes(pokemon.name)
+            );
           } else if (value === false) {
-            filteredData = filteredData.filter((pokemon) => !favourites.includes(pokemon.name))
+            filteredData = filteredData.filter(
+              (pokemon) => !favourites.includes(pokemon.name)
+            );
           }
-          break
+          break;
         }
       }
     }
 
     if (query) {
-      filteredData = filteredData.filter((pokemon) => pokemon.name.includes(query))
+      filteredData = filteredData.filter((pokemon) =>
+        pokemon.name.includes(query)
+      );
+    }
+
+    // This implementation isn't the best..but was the best i could come up with without manually redefining important project Type specifications.
+    if (selectedTypes.length > 0) {
+      filteredData = filteredData.filter(
+        (pokemon) =>
+          //@ts-ignore
+          selectedTypes.includes(pokemon.types[0] as PokemonType) ||
+          //@ts-ignore
+          (pokemon.types[1] &&
+            //@ts-ignore
+            selectedTypes.includes(pokemon.types[1] as PokemonType)) ||
+          //@ts-ignore
+          (pokemon.types[2] &&
+            //@ts-ignore
+            selectedTypes.includes(pokemon.types[2] as PokemonType))
+      );
     }
 
     filteredData.sort((a, b) => {
-      const aId = getIdFromUrl(a.url)
-      const bId = getIdFromUrl(b.url)
+      const aId = getIdFromUrl(a.url);
+      const bId = getIdFromUrl(b.url);
 
       if (aId > bId) {
-        return 1
+        return 1;
       } else {
-        return -1
+        return -1;
       }
-    })
+    });
 
-    setPokemon(filteredData)
-  }
+    setPokemon(filteredData);
+  };
 
   const fetchPokemon = async () => {
+    // had to 'enhance' each Pokemon with a types property to aid/simplify filtering
     try {
-      const response = await PokeAPI.Pokemon.list(150, 0)
-      setData(response.results)
-      setPokemon(response.results)
+      const response = await PokeAPI.Pokemon.list(150, 0);
+      const enhancedPokData = response.results.map(async (singleRes) => {
+        const singlePokemonData = await PokeAPI.Pokemon.resolve(singleRes.name);
+
+        const singlePokemonTypes = singlePokemonData.types.map(
+          (type) => type.type.name
+        );
+
+        return {
+          ...singleRes,
+          types: [...singlePokemonTypes]
+        };
+      });
+
+      Promise.all(enhancedPokData).then((res) => {
+        setData(res);
+        setPokemon(res);
+      });
     } catch (error) {
-      setError(error)
+      setError(error);
     }
-  }
+  };
+
+  const fetchPokemonTypes = async () => {
+    try {
+      const response = await PokeAPI.Type.list();
+      const types = response.results.map((item) => item.name);
+      setTypes(types as PokemonType[]);
+    } catch (error) {
+      setError(error);
+    }
+  };
 
   const search = (query: string) => {
-    setQuery(query)
-  }
+    setQuery(query);
+  };
+
+  const selectType = (type: PokemonType) => {
+    if (!selectedTypes.includes(type)) {
+      setSelectedTypes((prev) => [...prev, type]);
+    } else {
+      setSelectedTypes((prev) => prev.filter((item) => item !== type));
+    }
+  };
 
   function addFavourite(pokemon: INamedApiResource<IPokemon>) {
-    setFavourites([...favourites, pokemon.name])
+    setFavourites([...favourites, pokemon.name]);
   }
 
   function removeFavourite(pokemon: INamedApiResource<IPokemon>) {
-    setFavourites(favourites.filter((favourite) => favourite !== pokemon.name))
+    setFavourites(favourites.filter((favourite) => favourite !== pokemon.name));
   }
 
   function addFilter(field: Field, value: FilterValue) {
-    const newFilters = {...filters, [field]: value}
-    setFilters(newFilters)
+    const newFilters = { ...filters, [field]: value };
+    setFilters(newFilters);
   }
 
   function removeFilter(field: Field) {
-    const newFilters = {...filters}
-    newFilters[field] = undefined
-    setFilters(newFilters)
+    const newFilters = { ...filters };
+    newFilters[field] = undefined;
+    setFilters(newFilters);
   }
 
   if (error) {
-    return <div>Error</div>
+    return <div>Error</div>;
   }
 
   if (!pokemon) {
-    return <div></div>
+    return <div></div>;
   }
 
   return (
-    <PokemonContext.Provider value={{
-      pokemon,
-      query,
-      search,
-      favourites,
-      addFavourite,
-      removeFavourite,
-      filters,
-      addFilter,
-      removeFilter
-    }}>
+    <PokemonContext.Provider
+      value={{
+        pokemon,
+        query,
+        search,
+        selectType,
+        selectedTypes,
+        favourites,
+        addFavourite,
+        removeFavourite,
+        filters,
+        addFilter,
+        removeFilter,
+        types
+      }}
+    >
       {children}
     </PokemonContext.Provider>
-  )
-}
+  );
+};
 
 export const usePokemonContext = () => {
   const pokemon = useContext(PokemonContext);
 
   if (!pokemon) {
-    throw Error('Cannot use `usePokemonContext` outside of `PokemonProvider`');
+    throw Error("Cannot use `usePokemonContext` outside of `PokemonProvider`");
   }
 
   return pokemon;
-}
+};
 
 export default PokemonProvider;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,68 +1,94 @@
-import React, { ChangeEvent } from 'react'
-import { Container, Grid, InputAdornment, TextField, Typography, Box, Button, IconButton } from '@mui/material'
-import PokemonCard from '../components/PokemonCard'
-import { Field, usePokemonContext } from '../components/Contexts/PokemonProvider'
-import { Search, FavoriteBorder, Favorite, Close } from '@mui/icons-material'
+import React, { ChangeEvent } from "react";
+import {
+  Container,
+  Grid,
+  InputAdornment,
+  TextField,
+  Typography,
+  Box,
+  Button,
+  IconButton
+} from "@mui/material";
+import PokemonCard from "../components/PokemonCard";
+import PokemonTypeIcon from "../components/PokemonTypeIcon";
+import {
+  Field,
+  usePokemonContext
+} from "../components/Contexts/PokemonProvider";
+import { Search, FavoriteBorder, Favorite, Close } from "@mui/icons-material";
 
 const Home: React.FC = () => {
   const {
     pokemon,
     query,
     search,
+    selectType,
+    selectedTypes,
     favourites,
     addFavourite,
     removeFavourite,
     filters,
     addFilter,
     removeFilter,
-  } = usePokemonContext()
+    types
+  } = usePokemonContext();
 
   function handleQueryChange(event: ChangeEvent<HTMLInputElement>) {
-    search(event.target.value)
+    search(event.target.value);
   }
 
   const handleToggleFavourites = () => {
     if (filters[Field.favourite]) {
-      removeFilter(Field.favourite)
+      removeFilter(Field.favourite);
     } else {
-      addFilter(Field.favourite, true)
+      addFilter(Field.favourite, true);
     }
-  }
+  };
 
   return (
-    <Container maxWidth="lg" sx={{ py: 2 }}>
-      <Typography variant="h1">What Pokemon <br/>are you looking for?</Typography>
+    <Container maxWidth='lg' sx={{ py: 2 }}>
+      <Typography variant='h1'>
+        What Pokemon <br />
+        are you looking for?
+      </Typography>
       <Box
         sx={{
-          display: 'flex',
+          display: "flex",
           pt: 4,
           pb: 2
         }}
       >
         <TextField
-          id="pokemon-search"
-          placeholder="Search Pokemon"
-          variant="outlined"
+          id='pokemon-search'
+          placeholder='Search Pokemon'
+          variant='outlined'
           value={query}
           onChange={handleQueryChange}
           InputProps={{
             sx: { pr: 0 },
-            startAdornment: <InputAdornment position="start"><Search /></InputAdornment>,
-            endAdornment: <InputAdornment position="end">
-              <IconButton onClick={() => search('')}><Close /></IconButton>
-            </InputAdornment>
+            startAdornment: (
+              <InputAdornment position='start'>
+                <Search />
+              </InputAdornment>
+            ),
+            endAdornment: (
+              <InputAdornment position='end'>
+                <IconButton onClick={() => search("")}>
+                  <Close />
+                </IconButton>
+              </InputAdornment>
+            )
           }}
         />
 
         <Button
-          startIcon={filters[Field.favourite]
-            ? <Favorite />
-            : <FavoriteBorder />
+          startIcon={
+            filters[Field.favourite] ? <Favorite /> : <FavoriteBorder />
           }
-          color={filters[Field.favourite] ? 'primary' : 'secondary'}
+          color={filters[Field.favourite] ? "primary" : "secondary"}
           sx={{
             flexShrink: 0,
-            ml: '2rem'
+            ml: "2rem"
           }}
           onClick={handleToggleFavourites}
         >
@@ -70,15 +96,23 @@ const Home: React.FC = () => {
         </Button>
       </Box>
 
+      <Box sx={{ display: "flex", pb: 4, flexWrap: "wrap", gap: "8px" }}>
+        {types.map((type, idx) => (
+          <Button
+            key={type + idx}
+            color={selectedTypes.includes(type) ? "primary" : "secondary"}
+            onClick={() => {
+              selectType(type);
+            }}
+          >
+            <PokemonTypeIcon type={type} sx={{ mr: "8px" }} /> {type}
+          </Button>
+        ))}
+      </Box>
+
       <Grid container spacing={2}>
         {pokemon.map((pokemon) => (
-          <Grid
-            item
-            xs={12}
-            sm={6}
-            md={4}
-            key={pokemon.name}
-          >
+          <Grid item xs={12} sm={6} md={4} key={pokemon.name}>
             <PokemonCard
               pokemon={pokemon}
               isFavourite={favourites.includes(pokemon.name)}
@@ -89,7 +123,7 @@ const Home: React.FC = () => {
         ))}
       </Grid>
     </Container>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
